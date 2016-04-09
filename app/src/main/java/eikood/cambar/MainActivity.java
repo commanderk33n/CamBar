ddb01e0e696aee00639b698bba7faf26537a9d1f
eikood <hanfaplast@googlemail.com>
2016-04-09 17:33:40 +0200

    Initial commit

---
+++ app/src/main/java/eikood/cambar/MainActivity.java
@@ -0,0 +1,66 @@
+package eikood.cambar;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+// here we import the opencv content
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.objdetect.CascadeClassifier;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCamera;
+    private ImageView capturedImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+
+        capturedImage = (ImageView) findViewById(R.id.capturedImage);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            capturedImage.setImageBitmap(bp);
+        }
+    }
+
+    // start here with openCV opteration #capturedImage
+
+
+
+
+}
